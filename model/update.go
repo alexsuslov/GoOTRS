64e1fd0238c60863c19396c95ae15c0648089252
Alex Suslov <suslov@me.com>
2021-12-08 15:16:31 +0300

    AddUpdateDynamicField

--- model/update.go
+++ model/update.go
@@ -21,3 +21,25 @@ func (Update Update) ReaderCloser() io.ReadCloser {
 	data, _ := json.Marshal(Update)
 	return io.NopCloser(bytes.NewReader(data))
 }
+
+func (Update *Update) AddUpdateDynamicField(name string, value interface{}) {
+	if Update.DynamicFields ==nil{
+		Update.DynamicFields = []DynamicField{}
+	}
+	updated := false
+	for k, dField := range Update.DynamicFields{
+		if dField.Name==name{
+			Update.DynamicFields[k] = DynamicField{
+				name,
+				value,
+			}
+			updated = true
+			break
+		}
+	}
+	if !updated{
+		Update.DynamicFields = append(Update.DynamicFields)
+	}
+
+	Update.DynamicFields = append(Update.DynamicFields, DynamicField{name, value})
+}
